fb967360beb73c80801e9e7aa74f9b1fcedb470e
jemurdock <jessica.murdock@gmail.com>
2020-11-15 00:03:10 -0700

    book stores graph done

--- js/booksaleschart.js
+++ js/booksaleschart.js
@@ -7,7 +7,7 @@ class BookSalesChart{
     drawChart(){
         let g = d3.select("#bsales");
         g.append("text").attr("class", "graphtitle").attr("x", 300).attr("y", 50)
-            .text("Yearly Book Sales in Stores & Through E-Commerce")
+            .text("USA Yearly Book Sales in Stores & Through E-Commerce")
         let xScale = d3.scaleLinear().domain([1992,2018]).range([70,1040]);
         let xAxis = d3.axisBottom(xScale).ticks(13).tickFormat(function(d){
             return d;
