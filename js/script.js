48c37e7f8470b179e26ec5010fa5639a44ac13a3
jemurdock <jessica.murdock@gmail.com>
2020-11-14 20:14:23 -0700

    basic self-publish chart done

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ let selfpub;
 let numstores;
 
 d3.csv('./cleanData/BookSales.csv').then(function(data){
-    console.log(data);
     booksales = new BookSalesChart(data);
     booksales.drawChart();
 });
@@ -17,13 +16,11 @@ d3.csv('./cleanData/BookSales.csv').then(function(data){
 // });
 
 d3.csv('./cleanData/IndPub.csv').then(function(data){
-    console.log(data);
     selfpub = new SelfPublishChart(data);
     selfpub.drawChart();
 });
 
 d3.csv('./cleanData/BookStores.csv').then(function(data){
-    console.log(data);
     numstores = new StoresChart(data);
     numstores.drawChart();
 });
--- js/selfpublishchart.js
+++ js/selfpublishchart.js
@@ -16,17 +16,43 @@ class SelfPublishChart{
         let x = g.append("g").attr("class", "axis").attr("transform", "translate(15,680)").call(xAxis);
         x.append("text").attr("class", "axislabel").attr("x", 525).attr("y", 55).text("Year");
 
-        let yScale = d3.scaleLinear().domain([0,1.5]).range([630,5]);
-        let yAxis = d3.axisLeft(yScale).tickFormat(function(d){
-            if(d != "0")
-                return d;
+        //let yScale = d3.scaleLog().domain([1,1416384]).range([630,5]);
+        let yScale = d3.scaleLog().range([630,5]);
+        yScale.domain([1,1416384]).nice();
+        let yAxis = d3.axisLeft(yScale).tickFormat(function(d,i){
+            if(d > 1 && i%3 == 0)
+                return (d/10000).toFixed(1);             
             else
                 return "";
         });
         let y = g.append("g").attr("class", "axis").attr("transform", "translate(75,50)").call(yAxis);
         y.append("text").attr("class", "axislabel")
-            .attr("transform", "translate(-50,170) rotate(270)")
-            .text("Books Published (In Millions)");
+            .attr("transform", "translate(-50,120) rotate(270)")
+            .text("Books Published (In Tens of Thousands)");
 
+        let lineGenerator = d3.line()
+            .x(d => xScale(d.year)+15)
+            .y(d => yScale(d.published)+50);
+        let xlineChart = g.append("path").attr("class", "xlibris")
+            .attr("d", lineGenerator(this.data.filter(d => d.publisher === "XLIBRIS")));
+        let blineChart = g.append("path").attr("class", "blurb")
+            .attr("d", lineGenerator(this.data.filter(d => d.publisher === "BLURB")));
+        let clineChart = g.append("path").attr("class", "createspace")
+            .attr("d", lineGenerator(this.data.filter(d => d.publisher === "CREATESPACE")));
+
+        g.append("line").attr('x1', 750).attr('x2', 800).attr('y1', 500).attr('y2', 500)
+            .attr('class', 'xlibris');
+        g.append("text").attr('x', 805).attr('y',510).text("Published using Xlibris")
+            .attr("class", "legend");
+            
+        g.append("line").attr('x1', 750).attr('x2', 800).attr('y1', 550).attr('y2', 550)
+            .attr('class', 'blurb');
+        g.append("text").attr('x', 805).attr('y',560).text("Published using Blurb")
+            .attr("class", "legend");
+
+        g.append("line").attr('x1', 750).attr('x2', 800).attr('y1', 600).attr('y2', 600)
+            .attr('class', 'createspace');
+        g.append("text").attr('x', 805).attr('y',608).text("Published using Createspace")
+            .attr("class", "legend");
     }
 }
